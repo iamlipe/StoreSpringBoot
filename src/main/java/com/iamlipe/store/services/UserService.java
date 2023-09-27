bc452bf662160b442b316e46492521cc185689bc
Felipe Lima <felipe_lima10@icloud.com>
2023-09-27 15:05:32 -0300

    Exception handling - update

--- src/main/java/com/iamlipe/store/services/UserService.java
+++ src/main/java/com/iamlipe/store/services/UserService.java
@@ -4,6 +4,7 @@ import com.iamlipe.store.entities.User;
 import com.iamlipe.store.repositories.UserRepository;
 import com.iamlipe.store.services.exceptions.DatabaseException;
 import com.iamlipe.store.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = repository.getReferenceById(id);
-        updateDate(entity, user);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateDate(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateDate(User entity, User user) {
