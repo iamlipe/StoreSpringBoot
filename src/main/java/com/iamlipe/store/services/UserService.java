ac235fb4342211134de59e38025212b366c4e80b
Felipe Lima <felipe_lima10@icloud.com>
2023-09-27 13:40:19 -0300

    User insert

--- src/main/java/com/iamlipe/store/services/UserService.java
+++ src/main/java/com/iamlipe/store/services/UserService.java
@@ -22,4 +22,8 @@ public class UserService {
         Optional<User> user = repository.findById(id);
         return user.orElse(null);
     }
+
+    public User insert(User user) {
+        return repository.save(user);
+    }
 }
