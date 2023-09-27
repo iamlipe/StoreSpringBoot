ed131df97dae9d82bd47199be66bc86c4df475ba
Felipe Lima <felipe_lima10@icloud.com>
2023-09-27 00:54:15 -0300

    jpa repository, dependency injection, database seeding

---
+++ src/main/java/com/iamlipe/store/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.iamlipe.store.repositories;
+
+import com.iamlipe.store.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
