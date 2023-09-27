acd206fe184c384961cb4737e5a98eec0e67093d
Felipe Lima <felipe_lima10@icloud.com>
2023-09-27 12:31:31 -0300

    Fix Serializable

--- src/main/java/com/iamlipe/store/entities/Category.java
+++ src/main/java/com/iamlipe/store/entities/Category.java
@@ -2,18 +2,25 @@ package com.iamlipe.store.entities;
 
 import jakarta.persistence.*;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -37,6 +44,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
--- src/main/java/com/iamlipe/store/entities/User.java
+++ src/main/java/com/iamlipe/store/entities/User.java
@@ -3,6 +3,7 @@ package com.iamlipe.store.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.Objects;
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
