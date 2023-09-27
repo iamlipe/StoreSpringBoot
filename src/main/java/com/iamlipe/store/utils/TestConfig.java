c499061b767757f4402ee757ffbed209366c990f
Felipe Lima <felipe_lima10@icloud.com>
2023-09-27 13:01:21 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/iamlipe/store/utils/TestConfig.java
+++ src/main/java/com/iamlipe/store/utils/TestConfig.java
@@ -1,14 +1,8 @@
 package com.iamlipe.store.utils;
 
-import com.iamlipe.store.entities.Category;
-import com.iamlipe.store.entities.Product;
-import com.iamlipe.store.entities.User;
-import com.iamlipe.store.entities.Order;
+import com.iamlipe.store.entities.*;
 import com.iamlipe.store.entities.enums.OrderStatus;
-import com.iamlipe.store.repositories.CategoryRepository;
-import com.iamlipe.store.repositories.OrderRepository;
-import com.iamlipe.store.repositories.ProductRepository;
-import com.iamlipe.store.repositories.UserRepository;
+import com.iamlipe.store.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -65,5 +62,12 @@ public class TestConfig implements CommandLineRunner {
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
